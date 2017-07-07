6764e9e59087cf0be89ef13393c904a364f9e24b
Joe Parsley <joewparsley@gmail.com>
2017-07-07 11:30:58 -0700

    create animal.model with proper keys, and animal list component and is talking with rest of preoject

---
+++ app/animal-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Animal } from './animal.model';
+
+@Component ({
+  selector: 'animal-list',
+  template: `
+  <h1>hello did this work</h1>
+  <ul>
+    <li>
+    lolol
+    </li>
+  </ul>
+
+  `
+})
+
+export class AnimalListComponent {
+
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { Animal } from './animal.model';
 
 @Component({
   selector: 'app-root',
   template: `
-  <h1>Hey did this work???</h1>
+  <div class="container">
+    <h1>Animal Tracker!</h1>
+    <animal-list></animal-list>
+  </div>
   `
 })
 
 export class AppComponent {
+  masterAnimalList: Animal[] = [
+    new Animal('Arctic Fox', 'Moon', 2, 'Carinovore', 'Northern Trail', 5, 'female', 'cool shade', 'loud Noises'),
+    new Animal('Ocelot', 'Prince', 4, 'Carinovore', 'Tropical Forest', 6, 'male', 'Laying in the sun', 'toys that are not rope based'),
+    new Animal('Northwest Black Tailed Deer', 'Tinkerbell', 8, 'Herbivore', 'Northern Trail', 2, 'female', 'delicate roots and leaves', 'loud Noises')
+  ];
 
 }
+
+
+// <ul>
+//   <li></li>
+// </ul>
